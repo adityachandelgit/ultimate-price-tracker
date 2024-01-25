f37a86c5e1c7cc01171762d3e1d5683e9b0a1345
Aditya Chandel <aditya.chandel@digicert.com>
2024-01-24 20:42:15 -0700

    Fix amazon

--- src/main/java/com/adityachandel/ultimatepricetracker/stores/Amazon.java
+++ src/main/java/com/adityachandel/ultimatepricetracker/stores/Amazon.java
@@ -83,32 +83,34 @@ public class Amazon implements Store {
     }
 
     private String doGet(String url) throws IOException {
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
         Request request = new Request.Builder()
                 .url(url)
                 .method("GET", null)
                 .addHeader("authority", "www.amazon.com")
-                .addHeader("accept", "text/html,*/*")
+                .addHeader("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
                 .addHeader("accept-language", "en-US,en;q=0.9")
+                .addHeader("cache-control", "max-age=0")
                 .addHeader("cookie", cookieProperties.getAmazon())
                 .addHeader("device-memory", "8")
                 .addHeader("downlink", "10")
                 .addHeader("dpr", "2")
                 .addHeader("ect", "4g")
-                .addHeader("referer", "https://www.amazon.com/SAMSUNG-Unlocked-Smartphone-Adaptive-Lavender/dp/B0BLP3TL8J/?_encoding=UTF8&pd_rd_w=ve1As&content-id=amzn1.sym.5f7e0a27-49c0-47d3-80b2-fd9271d863ca%3Aamzn1.symc.e5c80209-769f-4ade-a325-2eaec14b8e0e&pf_rd_p=5f7e0a27-49c0-47d3-80b2-fd9271d863ca&pf_rd_r=D9XECQ5WFYYRZ2WRWGFC&pd_rd_wg=8mjfY&pd_rd_r=ec08118a-6b01-4f3d-a771-ba9e617eb87e&ref_=pd_gw_ci_mcx_mr_hp_atf_m&th=1")
                 .addHeader("rtt", "50")
                 .addHeader("sec-ch-device-memory", "8")
                 .addHeader("sec-ch-dpr", "2")
-                .addHeader("sec-ch-ua", "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"")
+                .addHeader("sec-ch-ua", "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"")
                 .addHeader("sec-ch-ua-mobile", "?0")
                 .addHeader("sec-ch-ua-platform", "\"macOS\"")
-                .addHeader("sec-ch-viewport-width", "838")
-                .addHeader("sec-fetch-dest", "empty")
-                .addHeader("sec-fetch-mode", "cors")
-                .addHeader("sec-fetch-site", "same-origin")
-                .addHeader("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
-                .addHeader("viewport-width", "838")
-                .addHeader("x-requested-with", "XMLHttpRequest")
+                .addHeader("sec-ch-viewport-width", "1035")
+                .addHeader("sec-fetch-dest", "document")
+                .addHeader("sec-fetch-mode", "navigate")
+                .addHeader("sec-fetch-site", "none")
+                .addHeader("sec-fetch-user", "?1")
+                .addHeader("upgrade-insecure-requests", "1")
+                .addHeader("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+                .addHeader("viewport-width", "1035")
                 .build();
         Response response = client.newCall(request).execute();
         return response.body().string();
