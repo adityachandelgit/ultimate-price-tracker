0692a9b6882c2230f273a226da2d0ed5079f0b58
Aditya Chandel <aditya.chandel@digicert.com>
2024-03-01 17:00:14 -0700

    Make init job scheduler to pick up newly added stores

--- src/main/java/com/adityachandel/ultimatepricetracker/service/quartz/JobScheduler.java
+++ src/main/java/com/adityachandel/ultimatepricetracker/service/quartz/JobScheduler.java
@@ -1,17 +1,17 @@
 package com.adityachandel.ultimatepricetracker.service.quartz;
 
-import com.adityachandel.ultimatepricetracker.model.entity.StateEntity;
-import com.adityachandel.ultimatepricetracker.model.enums.StateKey;
 import com.adityachandel.ultimatepricetracker.model.enums.StoreType;
 import com.adityachandel.ultimatepricetracker.repository.StateRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.quartz.*;
+import org.quartz.impl.matchers.GroupMatcher;
+import org.quartz.utils.Key;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-import java.util.TimeZone;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.time.ZoneId.systemDefault;
 import static org.quartz.CronScheduleBuilder.cronSchedule;
@@ -25,29 +25,25 @@ public class JobScheduler {
 
     @PostConstruct
     public void init() {
-        Optional<StateEntity> alreadyInitiated = stateRepository.findById(StateKey.INIT_QUARTZ_JOBS.name());
-        if (alreadyInitiated.isEmpty()) {
-            setupInitialJobs();
-            stateRepository.save(StateEntity.builder()
-                    .name(StateKey.INIT_QUARTZ_JOBS.name())
-                    .value("true")
-                    .build());
-        }
+        initNewJobs();
     }
 
     @SneakyThrows
-    void setupInitialJobs() {
-        for (StoreType storeType : StoreType.values()) {
-            JobDetail jobDetail = JobBuilder.newJob(FetchPriceJob.class)
-                    .withIdentity(storeType.name())
-                    .build();
-            Trigger trigger = TriggerBuilder.newTrigger()
-                    .withIdentity(storeType.name())
-                    .withSchedule(cronSchedule(storeType.getCron())
-                            .withMisfireHandlingInstructionFireAndProceed()
-                            .inTimeZone(TimeZone.getTimeZone(systemDefault())))
-                    .build();
-            scheduler.scheduleJob(jobDetail, trigger);
+    void initNewJobs() {
+        for (String storeName : Arrays.stream(StoreType.values()).map(StoreType::name).collect(Collectors.toSet())) {
+            Set<String> jobs = scheduler.getJobKeys(GroupMatcher.anyGroup()).stream().map(Key::getName).collect(Collectors.toSet());
+            if (!jobs.contains(storeName)) {
+                JobDetail jobDetail = JobBuilder.newJob(FetchPriceJob.class)
+                        .withIdentity(storeName)
+                        .build();
+                Trigger trigger = TriggerBuilder.newTrigger()
+                        .withIdentity(storeName)
+                        .withSchedule(cronSchedule(StoreType.valueOf(storeName).getCron())
+                                .withMisfireHandlingInstructionFireAndProceed()
+                                .inTimeZone(TimeZone.getTimeZone(systemDefault())))
+                        .build();
+                scheduler.scheduleJob(jobDetail, trigger);
+            }
         }
     }
 
